650a9b63a379497a19643538d57a2824050971fe
Bence Mihucza <mihben@gmail.com>
2026-07-31 15:38:37 +0200

    chore: Create slnx

---
+++ src/Ironyx.Testing/Helpers/FeatureAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Xunit.Sdk;
+
+namespace Ironyx.Testing
+{
+    [TraitDiscoverer("Ironyx.Testing.Helpers.FeatureTraitDiscoverer", "Ironyx.Testing")]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FeatureAttribute : Attribute, ITraitAttribute
+    {
+        public string Abbreviation { get; }
+        public string Feature { get; }
+
+        public FeatureAttribute(string abbreviation, string feature)
+        {
+            Abbreviation = abbreviation;
+            Feature = feature;
+        }
+    }
+}
---
+++ src/Ironyx.Testing/Helpers/FeatureTraitDiscoverer.cs
@@ -0,0 +1,21 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Ironyx.Testing.Helpers
+{
+    public class FeatureTraitDiscoverer : ITraitDiscoverer
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            string label = "NOT DEFINED";
+            var attributeInfo = traitAttribute as ReflectionAttributeInfo;
+            var featureAttribute = attributeInfo?.Attribute as FeatureAttribute;
+            if (featureAttribute != null)
+            {
+                label = $"{featureAttribute.Abbreviation} - {featureAttribute.Feature}";
+            }
+
+            yield return new KeyValuePair<string, string>("Feature", label);
+        }
+    }
+}
