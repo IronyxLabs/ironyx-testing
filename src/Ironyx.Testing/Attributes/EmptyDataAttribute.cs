5cb0d507e3bfb530b59c9216b5a30bdc1b2f96b5
Bence Mihucza <mihben@gmail.com>
2026-07-31 16:04:59 +0200

    feat: Empty inline data attribute (#3)
    * feat: Inline data attribute
    
    * chore: Set package description

---
+++ src/Ironyx.Testing/Attributes/EmptyDataAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Xunit.Sdk;
+
+namespace Ironyx.Testing
+{
+    /// <summary>
+    /// Inline data for null, empty string and whitespaces
+    /// </summary>
+    public class EmptyInlineDataAttribute : DataAttribute
+    {
+        private readonly IEnumerable<object[]> _data = [
+            [null!],
+            [string.Empty],
+            ["       "]
+        ];
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            return _data;
+        }
+    }
+}
